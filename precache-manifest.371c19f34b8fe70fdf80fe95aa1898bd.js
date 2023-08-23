cf31e99ba27878faa250671472b2a109f75e1215
omkar wakchaure <omkarwakchaure2019@example.com>
2023-08-23 10:06:26 +0530

    Updates

---
+++ precache-manifest.371c19f34b8fe70fdf80fe95aa1898bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b350234b5e4487a5da27",
+    "url": "/Reservation_App_UI/static/css/main.7a32b3ca.chunk.css"
+  },
+  {
+    "revision": "b350234b5e4487a5da27",
+    "url": "/Reservation_App_UI/static/js/main.b350234b.chunk.js"
+  },
+  {
+    "revision": "6b3d7ea36762fd6dc181",
+    "url": "/Reservation_App_UI/static/css/1.d9b25f27.chunk.css"
+  },
+  {
+    "revision": "6b3d7ea36762fd6dc181",
+    "url": "/Reservation_App_UI/static/js/1.6b3d7ea3.chunk.js"
+  },
+  {
+    "revision": "a9503797d8c02c98f6a5",
+    "url": "/Reservation_App_UI/static/js/runtime~main.a9503797.js"
+  },
+  {
+    "revision": "c0065c69a28a194b46a9083cef47ce51",
+    "url": "/Reservation_App_UI/index.html"
+  }
+];
